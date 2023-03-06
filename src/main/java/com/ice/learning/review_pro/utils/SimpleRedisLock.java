2ddbfd931d6d77e33b7fd11747ee335acd6508fc
Ice <wangice@petalmail.com>
2023-03-06 13:00:19 +0800

    全局锁

--- src/main/java/com/ice/learning/review_pro/utils/SimpleRedisLock.java
+++ src/main/java/com/ice/learning/review_pro/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.ice.learning.review_pro.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -12,11 +13,16 @@ import java.util.concurrent.TimeUnit;
  * @Desc:
  */
 
-public class SimpleRedisLock implements ILock{
+public class SimpleRedisLock implements ILock {
 
     private String name;
+
     private StringRedisTemplate template;
+
     private static final String KEY_PREFIX = "lock:";
+
+    // 注意：hutool包下的UUID方法
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate template) {
         this.name = name;
@@ -26,14 +32,21 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识
-        Long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
-        Boolean success = template.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+        Boolean success = template.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unLock() {
-        template.delete(KEY_PREFIX + name);
+        // 获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁标识
+        String id = template.opsForValue().get(KEY_PREFIX + name);
+        if (threadId.equals(id)) {
+            // 释放锁
+            template.delete(KEY_PREFIX + name);
+        }
     }
 }
